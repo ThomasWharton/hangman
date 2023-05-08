7c790a72dc45cf4f4c0fd4ab16fe4c563d80c90e
ThomasWharton <thomaswharton7@hotmail.com>
2023-05-08 22:01:16 +0000

    Added get_words function to words.py

--- words.py
+++ words.py
@@ -1,22 +1,37 @@
-backup_word_list = [
-    'congratulate',
-    'triumph',
-    'special',
-    'pushes',
-    'orange',
-    'prevent',
-    'opening',
-    'systems',
-    'obliterate',
-    'culture',
-    'computing',
-    'programme',
-    'beginning',
-    'populate',
-    'entertain',
-    'transform',
-    'apologetic',
-    'enormous',
-    'competitive',
-    'imagine'
-]
+import requests
+
+def get_words():
+    """
+    Fetches a list of words from api.
+    If fetch from api unsuccessful, uses backup list.
+    """
+    try:
+        response = requests.get('https://random-word-api.herokuapp.com/word?number=100')
+        word_list = response.json()
+        return word_list
+    except:
+        word_list = [
+        'congratulate',
+        'triumph',
+        'special',
+        'pushes',
+        'orange',
+        'prevent',
+        'opening',
+        'systems',
+        'obliterate',
+        'culture',
+        'computing',
+        'programme',
+        'beginning',
+        'populate',
+        'entertain',
+        'transform',
+        'apologetic',
+        'enormous',
+        'competitive',
+        'imagine'
+        ]
+        return word_list
+
+
