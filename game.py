72482d320d2bd2363ec15fa92d45e4404d94de01
ThomasWharton <thomaswharton7@hotmail.com>
2023-05-08 22:56:50 +0000

    Added while loop in game.py

--- game.py
+++ game.py
@@ -9,8 +9,45 @@ def start_game(word):
     letters_guessed = []
     words_guessed = []
     lives = 6
-    print("Hey name, lets play a game of Hangman!")
+    print("Hey name, lets play a game of Hangman!\n")
     print(display_hangman(lives))
     print(word_completion)
+    print("\n")
+
+    while not guessed_correctly and lives > 0:
+        guess = input("Guess a letter or a word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in letters_guessed:
+                print(f"Sorry! You already guessed the letter {guess}. Please try again.")
+            elif guess not in word:
+                print (f"Unlucky! {guess} is not in the word.")
+                lives -= 1
+                letters_guessed.append(guess)
+            else:
+                print (f"Well Done! {guess} is in the word")
+                letters_guessed.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed_correctly = True
+
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in words_guessed:
+                print(f"Sorry! You already guessed the word {guess}. Please try again.")
+            elif guess != word:
+                print(f"Unlucky! {guess} is not the word!")
+                lives -= 1
+                words_guessed.append(guess)
+            else:
+                guessed_correctly = True
+                word_completion = word
+
+        else:
+            print("Your guess is not valid. Please try again.")
+            print(display_hangman(lives))
+            print(word_completion)
+            print("\n")
 
 start_game(word)
