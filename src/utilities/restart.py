7e38b710e91d871bcaf031875a69771dcdc9dbb8
ThomasWharton <thomaswharton7@hotmail.com>
2023-05-15 14:26:20 +0000

    Restart: Add comment to function

--- src/utilities/restart.py
+++ src/utilities/restart.py
@@ -2,6 +2,10 @@ from src.utilities.words import get_word
 from src.game import start_game
 
 def restart_game():
+    """
+    Creates input to ask user if they would like to play again.
+    Restarts game or closes app depending on user input.
+    """
     input_valid = False
     while not input_valid:
         restart = input("Play again? (Y/N): ").upper()
