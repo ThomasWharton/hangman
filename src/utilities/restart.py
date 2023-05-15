b1e497879a58a93007599395aaf955c7ab118745
ThomasWharton <thomaswharton7@hotmail.com>
2023-05-15 14:16:39 +0000

    Restart: Remove unneccessary line

--- src/utilities/restart.py
+++ src/utilities/restart.py
@@ -10,7 +10,6 @@ def restart_game():
             start_game(word)
             input_valid = True
         elif restart[0] == "N":
-            input_valid = True
             exit()
         else:
             print("Input is invalid.\nPlease enter Y for yes and N for no.")
