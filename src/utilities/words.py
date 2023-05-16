7ed0215146ec6818aacbcb280a6292d59c26eb22
ThomasWharton <thomaswharton7@hotmail.com>
2023-05-16 12:28:25 +0000

    Words: Reformat code and add except reason

--- src/utilities/words.py
+++ src/utilities/words.py
@@ -1,6 +1,7 @@
 import requests
 import random
 
+
 def get_word():
     """
     Fetches a list of words from api and picks random word from it.
@@ -9,32 +10,30 @@ def get_word():
     try:
         response = requests.get('https://random-word-api.herokuapp.com/word?number=100')
         word_list = response.json()
-        word = random.choice(word_list).upper() 
+        word = random.choice(word_list).upper()
         return word
-    except:
+    except api_call_failure:
         word_list = [
-        'congratulate',
-        'triumph',
-        'special',
-        'pushes',
-        'orange',
-        'prevent',
-        'opening',
-        'systems',
-        'obliterate',
-        'culture',
-        'computing',
-        'programme',
-        'beginning',
-        'populate',
-        'entertain',
-        'transform',
-        'apologetic',
-        'enormous',
-        'competitive',
-        'imagine'
-        ]
-        word = random.choice(word_list).upper() 
+            'congratulate',
+            'triumph',
+            'special',
+            'pushes',
+            'orange',
+            'prevent',
+            'opening',
+            'systems',
+            'obliterate',
+            'culture',
+            'computing',
+            'programme',
+            'beginning',
+            'populate',
+            'entertain',
+            'transform',
+            'apologetic',
+            'enormous',
+            'competitive',
+            'imagine'
+            ]
+        word = random.choice(word_list).upper()
         return word
-
-
