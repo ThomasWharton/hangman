b8acaf0558f7b604203a1c3be43561fa9d41b130
ThomasWharton <thomaswharton7@hotmail.com>
2023-05-16 12:35:51 +0000

    Run: Reformat code

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
-# from src.utilities.welcome import welcome
-# from src.utilities.words import get_word
-# from src.game import start_game
-
 import src
+
 
 def main():
     username = src.welcome()
     print(f"Hey {username}, lets play a game of Hangman!\n")
     word = src.get_word()
-    src.start_game(word)  
+    src.start_game(word)
+
 
 main()
